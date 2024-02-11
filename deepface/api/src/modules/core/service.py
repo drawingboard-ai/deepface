d7cdf3bfab84527da1b540881eecc2069df2d865
Michael Carroll <michael@carroll.io>
2024-02-11 12:11:22 -0500

    Update service.py

--- deepface/api/src/modules/core/service.py
+++ deepface/api/src/modules/core/service.py
@@ -1,5 +1,9 @@
 from deepface import DeepFace
 
+import base64
+from tempfile import NamedTemporaryFile
+import os
+
 # pylint: disable=broad-except
 
 
@@ -37,18 +41,31 @@ def verify(
         return {"error": f"Exception while verifying: {str(err)}"}, 400
 
 
-def analyze(img_path, actions, detector_backend, enforce_detection, align):
+def analyze(base64_image, actions, detector_backend, enforce_detection, align):
     try:
         result = {}
+
+        image_data = base64.b64decode(base64_image)
+
+        with NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(image_data)
+            tmp_file_path = tmp_file.name
+
         demographies = DeepFace.analyze(
-            img_path=img_path,
+            img_path=tmp_file_path,
             actions=actions,
             detector_backend=detector_backend,
             enforce_detection=enforce_detection,
             align=align,
             silent=True,
         )
+
         result["results"] = demographies
+
+        os.remove(tmp_file_path)
+
         return result
     except Exception as err:
+        if 'tmp_file_path' in locals():
+            os.remove(tmp_file_path)
         return {"error": f"Exception while analyzing: {str(err)}"}, 400
